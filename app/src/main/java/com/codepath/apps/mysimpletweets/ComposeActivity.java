94982f9f4137b2196c303856c6c2b237310e63d0
Zahra Surani <zsurani@fb.com>
2016-06-30 16:24:50 -0700

    fourth commit

--- app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
@@ -58,4 +58,6 @@ public class ComposeActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
--- app/src/main/java/com/codepath/apps/mysimpletweets/fragments/TweetsListFragment.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/fragments/TweetsListFragment.java
@@ -3,6 +3,8 @@ package com.codepath.apps.mysimpletweets.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +12,17 @@ import android.widget.ListView;
 
 import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.TweetsArrayAdapter;
+import com.codepath.apps.mysimpletweets.TwitterApplication;
+import com.codepath.apps.mysimpletweets.TwitterClient;
 import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by zsurani on 6/27/16.
@@ -23,7 +32,8 @@ public class TweetsListFragment extends Fragment {
     protected TweetsArrayAdapter aTweets;
     protected ListView lvTweets;
     protected ArrayList<Tweet> tweets;
-
+    private SwipeRefreshLayout swipeContainer;
+    TwitterClient client;
 
 
     @Nullable
@@ -32,7 +42,22 @@ public class TweetsListFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_tweets_list, parent, false);
         lvTweets = (ListView) v.findViewById(R.id.lvTweets);
         lvTweets.setAdapter(aTweets);
-
+        swipeContainer = (SwipeRefreshLayout) v.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync(0);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
         return v;
     }
@@ -50,4 +75,34 @@ public class TweetsListFragment extends Fragment {
         aTweets.addAll(tweets);
         lvTweets.setSelection(0);
     }
+
+    public void fetchTimelineAsync(int page) {
+        client = TwitterApplication.getRestClient();
+        // Send the network request to fetch the updated data
+        // `client` here is an instance of Android Async HTTP
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                aTweets.clear();
+                aTweets.addAll(Tweet.fromJSONArray(response));
+                swipeContainer.setRefreshing(false);
+            }
+
+          /*  public void onSuccess(JSONArray json) {
+                // Remember to CLEAR OUT old items before appending in the new ones
+                aTweets.clear();
+                // ...the data has come back, add new items to your adapter...
+                aTweets.addAll(Tweet.fromJSONArray(json));
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }*/
+
+            public void onFailure(Throwable e) {
+                Log.d("DEBUG", "Fetch timeline error: " + e.toString());
+            }
+        });
+    }
+
 }
