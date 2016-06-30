52c2889d7cb331eec2d34303083ccca20e4b827e
Zahra Surani <zsurani@fb.com>
2016-06-30 16:36:23 -0700

    fifth commit

--- app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
@@ -1,10 +1,14 @@
 package com.codepath.apps.mysimpletweets;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.codepath.apps.mysimpletweets.models.User;
@@ -22,11 +26,47 @@ public class ComposeActivity extends AppCompatActivity {
     Tweet tweet;
     User u;
     EditText etPost;
+    TextView lblCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        etPost = (EditText) findViewById(R.id.etPost);
+        lblCount = (TextView)findViewById(R.id.lblCount);
+
+        // Attached Listener to Edit Text Widget
+        etPost.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // TODO Auto-generated method stub
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+
+            }
+
+            public void afterTextChanged(Editable s) {
+
+                // Display Remaining Character with respective color
+                int count = 140 - s.length();
+                lblCount.setText(Integer.toString(count));
+                lblCount.setTextColor(Color.GREEN);
+                if(count < 10)
+                {
+                    lblCount.setTextColor(Color.YELLOW);
+                }
+                if(count < 0)
+                {
+                    lblCount.setTextColor(Color.RED);
+                }
+
+            }
+        });
 
 
 //            @Override
@@ -43,7 +83,7 @@ public class ComposeActivity extends AppCompatActivity {
 
     public void onReturn(View view) {
 
-        etPost = (EditText) findViewById(R.id.etPost);
+
 
         client = TwitterApplication.getRestClient();
         client.postUserTimeline(etPost.getText().toString(), new JsonHttpResponseHandler() {
